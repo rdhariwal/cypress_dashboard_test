537e37afabf30c3139dd14b68a3f2f78b513cc3a
Rupinder <rupinderdhariwal@gmail.com>
2019-09-15 14:33:09 -0400

    break test

--- cypress/integration/custom/sample.spec.js
+++ cypress/integration/custom/sample.spec.js
@@ -2,6 +2,6 @@ describe('My First Test', function() {
     it('Visits the Kitchen Sink', function() {
         cy.visit('https://office.staging.move.mil')
         cy.get('.officeHeaderOne')
-            .contains("office.move.mil")
+            .contains("tsp.move.mil")
     })
 })
