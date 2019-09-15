dd4a279d83784d2f33af2c3d835ada1ff814c487
Rupinder <rupinderdhariwal@gmail.com>
2019-09-15 16:11:55 -0400

    fix broken test

--- cypress/integration/custom/sample.spec.js
+++ cypress/integration/custom/sample.spec.js
@@ -2,6 +2,6 @@ describe('My First Test', function() {
     it('Visits the Kitchen Sink', function() {
         cy.visit('https://office.staging.move.mil')
         cy.get('.officeHeaderOne')
-            .contains("tsp.move.mil")
+            .contains("office.move.mil")
     })
 })
